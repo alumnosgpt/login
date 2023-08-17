6780e97f83f31013ad0e462e2cfef594b5c0fcf6
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-17 08:57:10 -0600

    creacion index de menu

---
+++ views/menu/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+    <h1 class="text-center">BIENVENIDO</h1>
+    <div class="row justify-content-center mb-5">
+        <form class="col-lg-8 border bg-light p-3" id="formularioMenu">
+                <div class="col">
+                    <button type="button" id="btnVolver" class="btn btn-warning w-100">Volver</button>
+                </div>
+        </form>
+    </div>
+    <script src="<?= asset('./build/js/menu/index.js')  ?>"></script>
+</body>
+</html>
